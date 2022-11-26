042be225546f66b00868779f53325969a12b3d5e
Dmitrii Derenko <dima.16.artemov@gmail.com>
2022-11-27 00:50:54 +0300

    - added some info to loans

--- app/Http/Resources/Loan/LoanResource.php
+++ app/Http/Resources/Loan/LoanResource.php
@@ -19,13 +19,26 @@ class LoanResource extends JsonResource
      */
     public function toArray($request): array
     {
+        // TODO: refactor
+        $monthsLeft = $this->term - $this->first_payment->diff(now())->m - 1;
+
+        $left = $this->amount / $this->term * $monthsLeft;
+
+        $nextPayment = now()->day($this->first_payment->day);
+        if (now()->day >= $this->first_payment->day) {
+            $nextPayment->addMonth();
+        }
+
         return [
             'id' => $this->id,
             'name' => $this->name,
             'amount' => $this->amount,
+            'left' => round($left, 2),
+            'months_left' => $monthsLeft,
             'currency' => $this->currency->code,
             'term' => $this->term,
             'first_payment' => $this->first_payment->format('d.m.Y'),
+            'next_payment' => $nextPayment->format('d.m.Y'),
         ];
     }
 }
