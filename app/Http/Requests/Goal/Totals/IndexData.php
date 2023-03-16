c2ac48f2d137fa31bc3e728f0a58859f6a332418
Dmitrii Derenko <dima.16.artemov@gmail.com>
2023-03-16 15:24:38 +0300

    - fixed getting totals

--- app/Http/Requests/Goal/Totals/IndexData.php
+++ app/Http/Requests/Goal/Totals/IndexData.php
@@ -3,14 +3,15 @@
 namespace App\Http\Requests\Goal\Totals;
 
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Optional;
 
 class IndexData extends Data
 {
     /**
-     * @param array<string, array<string, float>> $courses
+     * @param array<string, array<string, float>>|Optional $courses
      */
     public function __construct(
-        public array $courses,
+        public array|Optional $courses,
     ) {
     }
 }
