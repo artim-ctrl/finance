198f16d0688e82428f769a7c475a271bb9f21bb9
Dmitrii Derenko <dima.16.artemov@gmail.com>
2023-03-18 17:19:52 +0300

    - fixed creating exchanges validation

--- app/Http/Requests/Exchange/StoreData.php
+++ app/Http/Requests/Exchange/StoreData.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\Exchange;
 
 use Spatie\LaravelData\Attributes\MapName;
 use Spatie\LaravelData\Attributes\Validation\Date;
+use Spatie\LaravelData\Attributes\Validation\Exists;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 
@@ -11,8 +12,10 @@ use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 class StoreData extends Data
 {
     public function __construct(
+        #[Exists('balances', 'id')]
         public int $balanceIdFrom,
         public float $amountFrom,
+        #[Exists('balances', 'id')]
         public int $balanceIdTo,
         public float $amountTo,
         #[Date]
