51e22846ee22aa09a96111767e5ab88faae7440b
Dmitrii Derenko <dmitrii.derenko@gmail.com>
2024-12-01 13:56:30 +0100

    installed react-router and set up typescript check

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: [
+            {
+                find: /^Containers/,
+                replacement: path.resolve(__dirname, 'src/Containers'),
+            },
+            {
+                find: /^Components/,
+                replacement: path.resolve(__dirname, 'src/Components'),
+            },
+        ],
+    },
 })
