2726356def5e525144ccf6ad9a1d2e1cbc6ec9ef
Dmitrii Derenko <dmitrii.derenko@gmail.com>
2024-12-07 21:09:28 +0100

    implemented monthly_expense_plans

--- frontend/Services/ExpenseApi.ts
+++ frontend/Services/ExpenseApi.ts
@@ -13,6 +13,15 @@ class ExpenseApi extends Api {
     async create(data: CreateExpenseData) {
         return await this.post('/v1/expenses', data)
     }
+
+    async updatePlan(data: {
+        year: number
+        month: number
+        categoryId: number
+        amount: number
+    }) {
+        return await this.put('/v1/expenses/plans', data)
+    }
 }
 
 export default new ExpenseApi(import.meta.env.VITE_BASE_API_URL)
--- internal/expenses/repositories/models.go
+++ internal/expenses/repositories/models.go
@@ -16,7 +16,8 @@ type ExpenseCategory struct {
 	UpdatedAt time.Time  `bun:"updated_at,default:current_timestamp" json:"updated_at"`
 	DeletedAt *time.Time `bun:"deleted_at" json:"deleted_at"`
 
-	Expenses []Expense `bun:"rel:has-many,join:id=expense_category_id" json:"expenses,omitempty"`
+	Expenses            []Expense            `bun:"rel:has-many,join:id=expense_category_id" json:"expenses,omitempty"`
+	MonthlyExpensePlans []MonthlyExpensePlan `bun:"rel:has-many,join:id=expense_category_id" json:"monthly_expense_plans,omitempty"`
 }
 
 type Expense struct {
@@ -32,4 +33,16 @@ type Expense struct {
 	DeletedAt         *time.Time `bun:"deleted_at" json:"deleted_at"`
 
 	ExpenseCategory ExpenseCategory `bun:"rel:belongs-to,join:expense_category_id=id" json:"expense_category"`
+}
+
+type MonthlyExpensePlan struct {
+	bun.BaseModel `bun:"table:monthly_expense_plans,alias:mep"`
+
+	ID                int64     `bun:",pk,autoincrement" json:"id"`
+	ExpenseCategoryID int64     `bun:"expense_category_id" json:"expense_category_id"`
+	Year              int       `bun:"year" json:"year"`
+	Month             int       `bun:"month" json:"month"`
+	Amount            float64   `bun:"amount" json:"amount"`
+	CreatedAt         time.Time `bun:"created_at,default:current_timestamp" json:"created_at"`
+	UpdatedAt         time.Time `bun:"updated_at,default:current_timestamp" json:"updated_at"`
 }
