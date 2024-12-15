5ef8e2dcd1e127637194b0d125c6d056af1b3911
Dmitrii Derenko <dmitrii.derenko@gmail.com>
2024-12-15 20:15:37 +0100

    Implemented creating income in selected month

--- internal/incomes/handlers/create.go
+++ internal/incomes/handlers/create.go
@@ -11,16 +11,34 @@ import (
 )
 
 type CreateRequest struct {
-	CategoryId   *int64  `json:"categoryId,omitempty" validate:"required_without=CategoryName"`
-	CategoryName *string `json:"categoryName,omitempty" validate:"required_without=CategoryId"`
-	Amount       float64 `json:"amount" validate:"required,gte=0.01"`
+	Date         time.Time `validate:"required"`
+	CategoryId   *int64    `json:"categoryId,omitempty" validate:"required_without=CategoryName"`
+	CategoryName *string   `json:"categoryName,omitempty" validate:"required_without=CategoryId"`
+	Amount       float64   `json:"amount" validate:"required,gte=0.01"`
 }
 
 func (h *Handler) CreateMapper(c *fiber.Ctx) error {
+	var (
+		year, month int
+		err         error
+	)
+
+	year, err = c.ParamsInt("year")
+	if err != nil {
+		return response.Error(c, "Couldn't parse year: "+err.Error())
+	}
+
+	month, err = c.ParamsInt("month")
+	if err != nil {
+		return response.Error(c, "Couldn't parse month: "+err.Error())
+	}
+
 	var req CreateRequest
-	if err := c.BodyParser(&req); err != nil {
+	if err = c.BodyParser(&req); err != nil {
 		return response.Error(c, "Couldn't parse request body: "+err.Error())
 	}
+
+	req.Date = time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
 
 	errs := h.validator.ValidateStruct(req)
 	if errs != nil {
@@ -56,7 +74,7 @@ func (h *Handler) Create(c *fiber.Ctx) error {
 
 	income := &repositories.Income{
 		IncomeCategoryID: categoryId,
-		Date:             time.Now().UTC(),
+		Date:             req.Date,
 		Amount:           req.Amount,
 	}
 
