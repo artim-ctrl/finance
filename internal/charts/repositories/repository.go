9754b073a4b62e1f36f13fb2916f0747e41d663f
Dmitrii Derenko <dmitrii.derenko@gmail.com>
2024-12-28 23:42:11 +0100

    Improved query to get info for charts

--- internal/charts/repositories/repository.go
+++ internal/charts/repositories/repository.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	"context"
+	"time"
 
 	"github.com/artim-ctrl/finance/internal/database/postgres"
 )
@@ -15,32 +16,31 @@ func NewRepository(db *postgres.Conn) *Repository {
 }
 
 type ExpenseData struct {
-	Date                  string  `bun:"date" json:"date"`
-	Last30DaysExpense     float64 `bun:"last_30_days_expense" json:"last30Days"`
-	Previous30DaysExpense float64 `bun:"previous_30_days_expense" json:"previous30Days"`
+	Date           string  `bun:"date" json:"date"`
+	CurrentAmount  float64 `bun:"current_amount" json:"currentAmount"`
+	PreviousAmount float64 `bun:"previous_amount" json:"previousAmount"`
 }
 
-func (r *Repository) Get(ctx context.Context) ([]ExpenseData, error) {
+func (r *Repository) Get(ctx context.Context, from, to, fromPrev, toPrev time.Time) ([]ExpenseData, error) {
 	var expenses []ExpenseData
 	err := r.db.NewSelect().
-		With("last_30_days", r.db.NewSelect().
-			ColumnExpr("date, SUM(amount) AS total_expense").
-			Table("expenses").
-			Where("date >= CURRENT_DATE - INTERVAL '30 days'").
-			Where("date < CURRENT_DATE").
-			Group("date")).
-		With("previous_30_days", r.db.NewSelect().
-			ColumnExpr("date, SUM(amount) AS total_expense").
-			Table("expenses").
-			Where("date >= CURRENT_DATE - INTERVAL '60 days'").
-			Where("date < CURRENT_DATE - INTERVAL '30 days'").
-			Group("date")).
-		Table("last_30_days").
-		Join("LEFT JOIN previous_30_days ON (last_30_days.date - interval '30' day) = previous_30_days.date").
-		ColumnExpr("DATE(last_30_days.date) AS date").
-		ColumnExpr("COALESCE(last_30_days.total_expense, 0) AS last_30_days_expense").
-		ColumnExpr("COALESCE(previous_30_days.total_expense, 0) AS previous_30_days_expense").
-		Order("last_30_days.date").
+		With("current_dates", r.db.NewSelect().
+			ColumnExpr("DATE(GENERATE_SERIES(?, ?, INTERVAL '1' DAY)) AS date", from.Format(time.DateOnly), to.Format(time.DateOnly))).
+		With("current", r.db.NewSelect().
+			ColumnExpr("cd.date, ROW_NUMBER() OVER (ORDER BY cd.date) AS num, SUM(e.amount) AS amount").
+			TableExpr("current_dates cd").
+			Join("LEFT JOIN expenses e ON e.date = cd.date").
+			Group("cd.date")).
+		With("previous_dates", r.db.NewSelect().
+			ColumnExpr("DATE(GENERATE_SERIES(?, ?, INTERVAL '1' DAY)) AS date", fromPrev.Format(time.DateOnly), toPrev.Format(time.DateOnly))).
+		With("previous", r.db.NewSelect().
+			ColumnExpr("pd.date, ROW_NUMBER() OVER (ORDER BY pd.date) AS num, SUM(e.amount) AS amount").
+			TableExpr("previous_dates pd").
+			Join("LEFT JOIN expenses e ON e.date = pd.date").
+			Group("pd.date")).
+		TableExpr("current c").
+		ColumnExpr("c.date, COALESCE(c.amount, 0) AS current_amount, COALESCE(p.amount, 0) AS previous_amount").
+		Join("LEFT JOIN previous p ON c.num = p.num").
 		Scan(ctx, &expenses)
 	if err != nil {
 		return nil, err
