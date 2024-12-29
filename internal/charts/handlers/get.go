493b54ef0f707168a69a2ece2ff673b1ed1bc0e8
Dmitrii Derenko <dmitrii.derenko@gmail.com>
2024-12-29 16:46:42 +0100

    Fixed getting charts info

--- internal/charts/handlers/get.go
+++ internal/charts/handlers/get.go
@@ -5,17 +5,20 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 
+	"github.com/artim-ctrl/finance/internal/models"
 	"github.com/artim-ctrl/finance/internal/servers/http/response"
 )
 
 func (h *Handler) Get(c *fiber.Ctx) error {
+	user := c.Locals("user").(*models.User)
+
 	from := time.Date(2024, time.December, 1, 0, 0, 0, 0, time.UTC)
 	to := time.Date(2024, time.December, 31, 0, 0, 0, 0, time.UTC)
 
 	fromPrev := time.Date(2024, time.November, 1, 0, 0, 0, 0, time.UTC)
 	toPrev := time.Date(2024, time.November, 30, 0, 0, 0, 0, time.UTC)
 
-	expenses, err := h.repo.Get(c.UserContext(), from, to, fromPrev, toPrev)
+	expenses, err := h.repo.Get(c.UserContext(), user.ID, from, to, fromPrev, toPrev)
 	if err != nil {
 		return response.Error(c, err.Error())
 	}
