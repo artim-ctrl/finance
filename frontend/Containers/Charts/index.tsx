388c6902f5ce76b4e8be5dec15577d31dd1e8aef
Dmitrii Derenko <dmitrii.derenko@gmail.com>
2024-12-30 15:05:38 +0100

    Implemented ability to select categories on Charts page

--- frontend/Containers/Charts/index.tsx
+++ frontend/Containers/Charts/index.tsx
@@ -1,44 +1,80 @@
 import { useEffect, useState } from 'react'
 import { LineChart } from '@mantine/charts'
 import ChartsApi from 'Services/ChartsApi'
-import { Container, Title } from '@mantine/core'
+import { Chip, Container, Group, Title } from '@mantine/core'
+
+interface Category {
+    id: number
+    name: string
+}
 
 interface Expense {
     date: string
-    last30Days: string
-    previous30Days: string
+    currentAmount: string
+    previousAmount: string
 }
 
 const Charts = () => {
     const [expensesData, setExpensesData] = useState<Expense[]>([])
+    const [categories, setCategories] = useState<Category[]>([])
+    const [selectedCategories, setSelectedCategories] = useState<string[]>([])
     const [isLoading, setIsLoading] = useState<boolean>(true)
     const [error, setError] = useState<string | null>(null)
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setExpensesData((await ChartsApi.getExpenses()) as Expense[])
-            } catch {
-                setError('Error fetching expenses data')
-            } finally {
-                setIsLoading(false)
+    const fetchData = async (categories: string[] | null = null) => {
+        try {
+            const response = (await ChartsApi.getExpenses(categories)) as {
+                expenses: Expense[]
+                categories: Category[]
             }
-        }
 
-        fetchData()
-    }, [])
+            setExpensesData(response.expenses as Expense[])
+            setCategories(response.categories as Category[])
+        } catch {
+            setError('Error fetching expenses data')
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchData().then(() => {
+            setSelectedCategories(categories.map(({ id }) => String(id)))
+        })
+    }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+    const onSelectCategory = (value: string[]) => {
+        setSelectedCategories(value)
+
+        fetchData(value)
+    }
 
     if (isLoading) {
         return <div>Loading...</div>
     }
 
-    if (error) {
+    if (error !== null) {
         return <div>{error}</div>
     }
 
     return (
         <Container size="xl" mt="md">
             <Title order={2}>Expense Trends</Title>
+
+            <Chip.Group
+                multiple
+                value={selectedCategories}
+                onChange={onSelectCategory}
+            >
+                <Group justify="center">
+                    {categories.map(({ id, name }, i) => (
+                        <Chip key={i} variant="outline" value={String(id)}>
+                            {name}
+                        </Chip>
+                    ))}
+                </Group>
+            </Chip.Group>
+
             <LineChart
                 h={400}
                 data={expensesData}
--- internal/charts/repositories/repository.go
+++ internal/charts/repositories/repository.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"time"
 
+	"github.com/uptrace/bun"
+
 	"github.com/artim-ctrl/finance/internal/database/postgres"
 )
 
@@ -15,33 +17,75 @@ func NewRepository(db *postgres.Conn) *Repository {
 	return &Repository{db: db}
 }
 
+func (r *Repository) GetCategories(ctx context.Context, userId int64, from, to, fromPrev, toPrev time.Time) ([]ExpenseCategory, error) {
+	var categories []ExpenseCategory
+	err := r.db.NewSelect().
+		ColumnExpr("DISTINCT ec.id, ec.name").
+		Model((*ExpenseCategory)(nil)).
+		Join("JOIN expenses e ON e.expense_category_id = ec.id").
+		Where("ec.user_id = ?", userId).
+		WhereGroup(" AND ", func(query *bun.SelectQuery) *bun.SelectQuery {
+			return query.
+				Where("e.date BETWEEN ? AND ?", from, to).
+				WhereOr("e.date BETWEEN ? AND ?", fromPrev, toPrev)
+		}).
+		Scan(ctx, &categories)
+	if err != nil {
+		return nil, err
+	}
+
+	return categories, nil
+}
+
 type ExpenseData struct {
 	Date           string  `bun:"date" json:"date"`
 	CurrentAmount  float64 `bun:"current_amount" json:"currentAmount"`
 	PreviousAmount float64 `bun:"previous_amount" json:"previousAmount"`
 }
 
-func (r *Repository) Get(ctx context.Context, userId int64, from, to, fromPrev, toPrev time.Time) ([]ExpenseData, error) {
+func (r *Repository) Get(
+	ctx context.Context,
+	categories []int64,
+	userId int64,
+	from,
+	to,
+	fromPrev,
+	toPrev time.Time,
+) ([]ExpenseData, error) {
+	currentSub := r.db.NewSelect().
+		ColumnExpr("cd.date, ROW_NUMBER() OVER (ORDER BY cd.date) AS num, SUM(e.amount) AS amount").
+		TableExpr("current_dates cd").
+		Join("LEFT JOIN expenses e ON e.date = cd.date").
+		Join("LEFT JOIN expense_categories ec ON ec.id = e.expense_category_id").
+		Where("ec.user_id = ?", userId).
+		Group("cd.date")
+
+	if len(categories) > 0 {
+		currentSub = currentSub.
+			Where("ec.id IN (?)", bun.In(categories))
+	}
+
+	previousSub := r.db.NewSelect().
+		ColumnExpr("pd.date, ROW_NUMBER() OVER (ORDER BY pd.date) AS num, SUM(e.amount) AS amount").
+		TableExpr("previous_dates pd").
+		Join("LEFT JOIN expenses e ON e.date = pd.date").
+		Join("LEFT JOIN expense_categories ec ON ec.id = e.expense_category_id").
+		Where("ec.user_id = ?", userId).
+		Group("pd.date")
+
+	if len(categories) > 0 {
+		previousSub = previousSub.
+			Where("ec.id IN (?)", bun.In(categories))
+	}
+
 	var expenses []ExpenseData
 	err := r.db.NewSelect().
 		With("current_dates", r.db.NewSelect().
 			ColumnExpr("DATE(GENERATE_SERIES(?, ?, INTERVAL '1' DAY)) AS date", from.Format(time.DateOnly), to.Format(time.DateOnly))).
-		With("current", r.db.NewSelect().
-			ColumnExpr("cd.date, ROW_NUMBER() OVER (ORDER BY cd.date) AS num, SUM(e.amount) AS amount").
-			TableExpr("current_dates cd").
-			Join("LEFT JOIN expenses e ON e.date = cd.date").
-			Join("LEFT JOIN expense_categories ec ON ec.id = e.expense_category_id").
-			Where("ec.user_id = ?", userId).
-			Group("cd.date")).
+		With("current", currentSub).
 		With("previous_dates", r.db.NewSelect().
 			ColumnExpr("DATE(GENERATE_SERIES(?, ?, INTERVAL '1' DAY)) AS date", fromPrev.Format(time.DateOnly), toPrev.Format(time.DateOnly))).
-		With("previous", r.db.NewSelect().
-			ColumnExpr("pd.date, ROW_NUMBER() OVER (ORDER BY pd.date) AS num, SUM(e.amount) AS amount").
-			TableExpr("previous_dates pd").
-			Join("LEFT JOIN expenses e ON e.date = pd.date").
-			Join("LEFT JOIN expense_categories ec ON ec.id = e.expense_category_id").
-			Where("ec.user_id = ?", userId).
-			Group("pd.date")).
+		With("previous", previousSub).
 		TableExpr("current c").
 		ColumnExpr("c.date, COALESCE(c.amount, 0) AS current_amount, COALESCE(p.amount, 0) AS previous_amount").
 		Join("LEFT JOIN previous p ON c.num = p.num").
